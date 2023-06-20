6711f0d0a6ef338878930e06198d4972f93c6f65
Гаджи Шахбанов <119418259+TheXimik-DA@users.noreply.github.com>
2023-06-20 12:50:50 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALOWED_HOSTS = ['84.252.139.221', '127.0.0.1', 'localhost', 'ximikittygramm.sytes.net']
 
